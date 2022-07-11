14337bdc2d578501c70b52f76e9b92f8ea3329fa
Radmir <yaradmir2203@gmail.com>
2022-07-11 21:15:13 +0500

    Initial commit

---
+++ src/test/java/cloud/autotests/tests/JobSearchTest.java
@@ -0,0 +1,62 @@
+package cloud.autotests.tests;
+
+import cloud.autotests.helpers.DriverUtils;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class JobSearchTest extends TestBase {
+    @Test
+    @DisplayName("Test Website НОВЕО")
+    void generatedTest() {
+        step("Open https://xn--b1agwec.xn--p1ai/", () ->
+            open("https://xn--b1agwec.xn--p1ai/"));
+
+        step("Click search", () ->
+            $(".js-header-search-btn .icon-svg").click());
+
+        step("Input Тестировщик", () ->
+            $("[id=search]").setValue("Тестировщик").pressEnter());
+
+        step("Job selection", () ->
+            $(".search-page__found:nth-child(2) .search-page__found-title").click());
+
+        step("Should be Тестировщик", () ->
+            $(".vacancies-details__title").shouldHave(text("Тестировщик")));
+    }
+
+    @Test
+    @DisplayName("Page title should have header text")
+    void titleTest() {
+        step("Open url 'https://xn--b1agwec.xn--p1ai/'", () ->
+            open("https://xn--b1agwec.xn--p1ai/"));
+
+        step("Page title should have text 'Новео'", () -> {
+            String expectedTitle = "Новео";
+            String actualTitle = title();
+
+            assertThat(actualTitle).isEqualTo(expectedTitle);
+        });
+    }
+
+    @Test
+    @DisplayName("Page console log should not have errors")
+    void consoleShouldNotHaveErrorsTest() {
+        step("Open url 'https://xn--b1agwec.xn--p1ai/'", () ->
+            open("https://xn--b1agwec.xn--p1ai/"));
+
+        step("Console logs should not contain text 'SEVERE'", () -> {
+            String consoleLogs = DriverUtils.getConsoleLogs();
+            String errorText = "SEVERE";
+
+            assertThat(consoleLogs).doesNotContain(errorText);
+        });
+    }
+}
